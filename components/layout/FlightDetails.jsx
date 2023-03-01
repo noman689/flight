c2b30a808edf84b76e45ff719ea38d67806e7ca7
jahanzaib2603 <jahanzaib@clustox.com>
2023-03-02 01:15:46 +0500

    scss file is added

--- components/layout/FlightDetails.jsx
+++ components/layout/FlightDetails.jsx
@@ -8,13 +8,14 @@ import {
   DiffOutlined,
   ArrowDownOutlined
 } from '@ant-design/icons';
+import './FlightDetails.scss';
 
 const FlightDetails = ({ fromDate = "02-02-2021", toDate = "03-02-2021", departure = "Karachi", departureSub = "KTC", destination = "Lahore", destinationSub = "Lhr", plan = "1 passenger", type = "Return", departureImg = "https://source.unsplash.com/1600x900/?lahore" }) => {
   return (
 
-    <Row style={{ display: 'flex', width: '80vw', background: 'white', height: '220px', borderRadius: '10px', marginTop: '20px' }} justify="space-between">
+    <Row className="flightDetails" justify="space-between">
       <Col xs={24} sm={24} md={3} lg={3}>
-        <img height={'100%'} width='100%' src={departureImg} style={{ objectFit: 'cover' }} />
+        <img height={'100%'} width='100%' src={departureImg} className="imageCover" />
       </Col>
       <Col xs={24} sm={24} md={21} lg={21} style={{ padding: '20px' }}>
         <Row style={{ marginBottom: '30px' }}>
@@ -22,13 +23,13 @@ const FlightDetails = ({ fromDate = "02-02-2021", toDate = "03-02-2021", departu
           <span style={{ marginInline: '5px' }}>-</span>
           <span>{moment.utc(toDate).format("DD MMM YYYY")}</span>
         </Row >
-        <Row justify={'space-between'} style={{ marginBottom: '30px', borderBottom: '1px solid grey', paddingBottom: '10px' }}  >
-          <Col xs={8} sm={12} md={2} lg={1} style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <Row justify={'space-between'} className="departureCard"  >
+          <Col xs={8} sm={12} md={2} lg={2} style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <Col xs={24} sm={24} md={20} lg={20} style={{ fontSize: '30px', fontWeight: '500' }}>{departureSub}</Col>
             <Col xs={24} sm={24} md={24} lg={24}>{departure}</Col>
           </Col>
           <Col xs={4} sm={12} md={4} lg={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>  <ArrowRightOutlined /> </Col>
-          <Col xs={8} sm={12} md={5} lg={1}>
+          <Col xs={8} sm={12} md={5} lg={2}>
             <Col xs={24} sm={24} md={20} lg={20} style={{ fontSize: '30px', fontWeight: '500' }}>{destinationSub}</Col>
             <Col xs={24} sm={24} md={24} lg={24}>{destination}</Col>
           </Col>
