fe170c1b5594e64016be2b850a3120f1dc52266b
maaz <maaziqbal631@gmail.com>
2023-04-10 13:50:19 +0500

    test

--- src/components/smart/HomePage/HomePage.tsx
+++ src/components/smart/HomePage/HomePage.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import SearchFlightCard from '../BookingCard/BookingCard';
 import DestinationsFlicker from '../DestinationsFlicker/DestinationsFlicker';
 import DisplayCards from '../DisplayCards/DisplayCards';
-import './HomePage.scss'
+import './HomePage.scss';
 
 const HomePage = () => {
   return (
-    <div className='content-section'>
+    <div className="content-section">
       <SearchFlightCard />
       <DisplayCards />
-      <DestinationsFlicker/>
+      <DestinationsFlicker />
     </div>
   );
 };
