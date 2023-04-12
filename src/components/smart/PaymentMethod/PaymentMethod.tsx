ff8c3117da834fe449f1d332b2df48e6d717585b
uzairajml <uzairajmal47@gmail.com>
2023-04-12 15:43:05 +0500

    fix card payment intent issue

--- src/components/smart/PaymentMethod/PaymentMethod.tsx
+++ src/components/smart/PaymentMethod/PaymentMethod.tsx
@@ -1,21 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { CardPayment } from '@duffel/components';
-import '@duffel/components/dist/CardPayment.min.css';
+import { paymentIntentAPI } from '@client/services/paymentService';
 
 const PaymentMethod = () => {
-    const successfulPaymentHandlerFn = () => {
-      // Show 'successful payment' page and confirm Duffel PaymentIntent
+  useEffect(() => {
+    const getPaymentIntent = async () => {
+      try {
+        const data = await paymentIntentAPI({
+          total_amount: '12.00',
+          total_currency: 'USD',
+        });
+        console.log('data', data);
+      } catch (error) {
+        console.log('error', error);
+      }
     };
+    getPaymentIntent();
+  }, []);
+  const successfulPaymentHandlerFn = () => {
+    // Show 'successful payment' page and confirm Duffel PaymentIntent
+  };
 
-    const errorPaymentHandlerFn = () => {
-      // Show error page
-    };
+  const errorPaymentHandlerFn = () => {
+    // Show error page
+  };
   return (
     <div>
       <CardPayment
-        duffelPaymentIntentClientToken={'c2hramgzaGVsbG8gd29ybGQgIyMgZ2lyYWY='}
-        // successfulPaymentHandler={successfulPaymentHandlerFn}
-        // errorPaymentHandler={errorPaymentHandlerFn}
+        duffelPaymentIntentClientToken={
+          'eyJjbGllbnRfc2VjcmV0IjoicGlfMUl5YTBiQW5rMVRkeXJvRE1iWkJPN0ZSX3NlY3JldF9TbGFrYnJjYnFHZGZha2VrcjdCNE5jZWVyIiwicHVibGlzaGFibGVfa2V5IjoicGtfbGl2ZV81MUl0Q3YwQW5rMUdkeXJvRFlFU3M3RnBTUEdrNG9kbDhneDF3Y1RBNVEzaUcyWEFWVEhxdFlKSVhWMUxoSU5GQUtFMjA1dFdmRGVIcXhwUVdnYkIzTkVFbzAwMmdVY1hzR0YifQ=='
+        }
+        successfulPaymentHandler={successfulPaymentHandlerFn}
+        errorPaymentHandler={errorPaymentHandlerFn}
       />
     </div>
   );
