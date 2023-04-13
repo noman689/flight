7df26c7b638d4199344c0af56d9b0fa8729f773e
jahanzaib2630 <jahanzaib@clustox.com>
2023-04-14 01:21:47 +0500

    UI Completed

--- src/components/smart/FlightTicket/FlightTicket.tsx
+++ src/components/smart/FlightTicket/FlightTicket.tsx
@@ -6,26 +6,26 @@ import barCode from '../../../assets/barCode.svg';
 import './FlightTicket.scss';
 
 const FlightTicket = () => {
-  const [encodedData, setEncodedData] = useState('')
-  const [meta, setMeta] = useState('')
+    const [encodedData, setEncodedData] = useState('')
+    const [meta, setMeta] = useState('')
 
-  useEffect(() => {
-    debugger
-    const encodedData = (window.location.href.split('=')[1]);
-    setMeta(JSON.parse(decodeURIComponent(encodedData)));
+    useEffect(() => {
 
-  }, [window.location.href])
+      const encodedData = (window.location.href.split('=')[1]);
+      setMeta(JSON.parse(decodeURIComponent(encodedData)));
 
-  console.log(meta)
+    }, [window.location.href])
+
+    console.log(meta)
   return (
     <div style={{ width: '100%' }}>
       <Row className="main" justify={'center'}>
         <Col
           xs={24}
           sm={24}
-          md={8}
-          lg={8}
-          className="flexCenter borderTopLeftRadius dottedBorder"
+          md={12}
+          lg={10}
+          className="flexCenter borderTopLeftRadius smallscreen-borderrighttop dottedBorder"
         >
           <img src={planeImage} className="planeImage" />
 
@@ -34,9 +34,9 @@ const FlightTicket = () => {
         <Col
           xs={24}
           sm={24}
-          md={5}
-          lg={5}
-          className="flexCenter borderTopRightRadius"
+          md={9}
+          lg={6}
+          className="flexCenter borderTopRightRadius hideOnSmallScreens"
         ></Col>
       </Row>
 
@@ -44,8 +44,8 @@ const FlightTicket = () => {
         <Col
           xs={24}
           sm={24}
-          md={8}
-          lg={8}
+          md={12}
+          lg={10}
           className="displayCenter Row2BorderCol1Top"
         >
           <div>
@@ -54,24 +54,24 @@ const FlightTicket = () => {
             <span className="font70">LHR</span>
           </div>
         </Col>
-        <Col xs={24} sm={24} md={5} lg={5} className="Row2BorderCol2Top">
-          <Row justify={'center'} className="rotate ">
-            <Col xs={24} sm={24} md={5} lg={8}>
-              <div className=" marginLeft10 fontSize20">
+        <Col xs={24} sm={24} md={9} lg={6} className="Row2BorderCol2Top">
+          <Row justify={'center'} className="rotate hideOnSmallScreens">
+            <Col xs={24} sm={24} md={24} lg={8}>
+              <div className=" marginLeft10 fontSize20 margin768 fontSizeOn768">
                 <span className="headingColor">Passenger</span>
                 <br />
                 <span>Jhon Jack</span>
               </div>
             </Col>
-            <Col xs={24} sm={24} md={5} lg={8}>
-              <div className=" marginLeft10 fontSize20">
+            <Col xs={24} sm={24} md={24} lg={8}>
+              <div className=" marginLeft10 fontSize20 margin768 fontSizeOn768">
                 <span className="headingColor">Gate</span>
                 <br />
                 <span>23A</span>
               </div>
             </Col>
-            <Col xs={24} sm={24} md={5} lg={24}>
-              <div className=" marginLeft10 fontSize20">
+            <Col xs={24} sm={24} md={24} lg={24}>
+              <div className=" marginLeft10 fontSize20 margin768 fontSizeOn768">
                 <span className="headingColor">Departure</span>
                 <br />
                 <span>06:55 12 DEC 2019</span>
@@ -84,12 +84,12 @@ const FlightTicket = () => {
         <Col
           xs={24}
           sm={24}
-          md={8}
-          lg={8}
-          className="displayCenter Row2BorderCol1"
+          md={12}
+          lg={10}
+          className="displayCenter Row2BorderCol1 smallScreen-borderRightBottom"
         >
-          <div className="footer-div ">
-            <div className="dFlex fontSize20">
+          <div className="footer-div">
+            <div className="dFlex fontSize20 size768">
               <div className=" marginLeft10">
                 <span className="headingColor">Passenger</span>
                 <br />
@@ -107,15 +107,21 @@ const FlightTicket = () => {
               </div>
             </div>
             <div>
-              <img style={{ width: '70px' }} src={barCode} />
+              <img className="barCode" src={barCode} />
             </div>
           </div>
         </Col>
-        <Col xs={24} sm={24} md={5} lg={5} className="Row2BorderCol2">
+        <Col
+          xs={24}
+          sm={24}
+          md={9}
+          lg={6}
+          className="Row2BorderCol2 hideOnSmallScreens"
+        >
           <Row>
             <Col xs={24} sm={24} md={5} lg={15}></Col>
             <Col xs={24} sm={24} md={5} lg={5}>
-              <img style={{ width: '70px' }} src={barCode} />
+              <img className="barCode" src={barCode} />
             </Col>
           </Row>
         </Col>
