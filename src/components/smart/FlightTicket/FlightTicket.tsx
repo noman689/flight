52b50c59f1081c5b746cb3b36aa041213a7ff927
maaz <maaziqbal631@gmail.com>
2023-04-17 05:11:48 +0500

    Flight ticket pdf

--- src/components/smart/FlightTicket/FlightTicket.tsx
+++ src/components/smart/FlightTicket/FlightTicket.tsx
@@ -1,20 +1,35 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
 // @ts-ignore
 import planeImageBlack from '../../../assets/ticketBlack.svg';
 import './FlightTicket.scss';
 import { isEmpty } from 'ramda';
 import moment from 'moment';
 import Barcode from 'react-barcode';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import FlightTicketPdf from '../FlightTicketPdf/FlightTicketPdf';
 
 const FlightTicket = () => {
+  const flightDetails = {
+    from: 'Lahore',
+    to: 'Dubai',
+    airline: 'Qatar',
+    flightNumber: '12212',
+    departureTerminal: 'Lahore',
+    arrivalTerminal: 'Dubai',
+    departureDate: '12 Aug Tues 2021',
+    departureTime: '12:40',
+    arrivalDate: '12 Aug Tues 2021',
+    arrivalTime: '15:40',
+    seatClass: 'Economy',
+    seatNumber: '56',
+  };
   const [ticketsData, setTicketsData] = useState([]);
 
   useEffect(() => {
-    const offerInfoString = localStorage.getItem("offerInfo");
+    const offerInfoString = localStorage.getItem('offerInfo');
     const meta = JSON.parse(offerInfoString);
     if (!isEmpty(meta)) {
-
       const dataObj = meta.confirmationDetails.data.offer?.data;
 
       dataObj.passengers.map((value) => {
@@ -35,22 +50,18 @@ const FlightTicket = () => {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const [screenHeight, setScreenHeight] = useState(window.innerHeight);
 
-  // Update screen size on window resize
   const handleResize = () => {
     setScreenWidth(window.innerWidth);
     setScreenHeight(window.innerHeight);
   };
 
   useEffect(() => {
-    // Add event listener for window resize
     window.addEventListener('resize', handleResize);
 
-    // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
-
 
   return (
     <div style={{ width: '100%' }}>
@@ -58,7 +69,7 @@ const FlightTicket = () => {
         ticketsData.map((item, index) => {
           return (
             <React.Fragment key={index}>
-              <Row className="main" justify={'center'} >
+              <Row className="main" justify={'center'}>
                 <Col
                   xs={24}
                   sm={24}
@@ -110,12 +121,16 @@ const FlightTicket = () => {
                   lg={screenWidth === 1024 ? 7 : 4}
                   className="Row2BorderCol2Top"
                 >
-                  <Row justify={'center'} className="rotate hideOnSmallScreens" align='bottom'>
+                  <Row
+                    justify={'center'}
+                    className="rotate hideOnSmallScreens"
+                    align="bottom"
+                  >
                     <Col xs={24} sm={24} md={24} lg={8}>
                       <div className=" invertedDivmarginLeft10 fontSize20 margin768 fontSizeOn768">
                         <span className="headingColor">Passenger</span>
                         <br />
-                        <span className='text-capitalize'>
+                        <span className="text-capitalize">
                           {item.passenger.given_name +
                             ' ' +
                             item.passenger.family_name}
@@ -143,7 +158,7 @@ const FlightTicket = () => {
                   </Row>
                 </Col>
               </Row>
-              <Row justify={'center'} align={'bottom'} >
+              <Row justify={'center'} align={'bottom'}>
                 <Col
                   xs={24}
                   sm={24}
@@ -152,24 +167,23 @@ const FlightTicket = () => {
                   className="displayCenter Row2BorderCol1 smallScreen-borderRightBottom"
                 >
                   <div className="footer-div">
-                    <Row dir='col' justify={'center'}>
+                    <Row dir="col" justify={'center'}>
                       <div className="divOverFlow2">
-                        <Col xs={24}
-                          sm={24}
-                          md={24}
-                          lg={24}>
-                          <Barcode value={item} displayValue={false} width={1} height={30} />
+                        <Col xs={24} sm={24} md={24} lg={24}>
+                          <Barcode
+                            value={item}
+                            displayValue={false}
+                            width={1}
+                            height={30}
+                          />
                         </Col>
                       </div>
-                      <Col xs={24}
-                        sm={24}
-                        md={24}
-                        lg={24}>
-                        <div className="dFlex fontSize20 size768" >
+                      <Col xs={24} sm={24} md={24} lg={24}>
+                        <div className="dFlex fontSize20 size768">
                           <div className=" marginLeft10">
                             <span className="headingColor">Passenger</span>
                             <br />
-                            <span className='text-capitalize'>
+                            <span className="text-capitalize">
                               {item.passenger.given_name +
                                 ' ' +
                                 item.passenger.family_name}
@@ -184,16 +198,15 @@ const FlightTicket = () => {
                             <span className="headingColor">Departure</span>
                             <br />
                             <span>
-                              {moment(item.slice.segments[0].departing_at).format(
-                                'HH:mm:ss YYYY-MM-DD',
-                              )}
+                              {moment(
+                                item.slice.segments[0].departing_at,
+                              ).format('HH:mm:ss YYYY-MM-DD')}
                             </span>
                           </div>
                         </div>
                       </Col>
                     </Row>
                   </div>
-
                 </Col>
                 <Col
                   xs={24}
@@ -211,12 +224,29 @@ const FlightTicket = () => {
                       style={{ textAlign: 'center', marginTop: '20px' }}
                     >
                       <div className="divOverFlow1">
-
-                        <Barcode value={item} displayValue={false} width={1} height={30} />
+                        <Barcode
+                          value={item}
+                          displayValue={false}
+                          width={1}
+                          height={30}
+                        />
                       </div>
                     </Col>
                   </Row>
                 </Col>
+              </Row>
+              <Row justify={'center'} style={{ marginTop: 40 }}>
+                <Button style={{ borderColor: '#701644' }}>
+                  <PDFDownloadLink
+                    document={<FlightTicketPdf {...flightDetails} />}
+                    fileName="e-ticket.pdf"
+                    style={{ textDecoration: 'none', color: '#701644' }}
+                  >
+                    {({ loading }) =>
+                      loading ? 'Loading document...' : 'Download PDF'
+                    }
+                  </PDFDownloadLink>
+                </Button>
               </Row>
             </React.Fragment>
           );
