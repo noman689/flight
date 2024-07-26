0ebe172af33c45e82e2258eaf5c47b04c8aaf68f
noman689 <62285695+noman689@users.noreply.github.com>
2024-07-26 19:03:36 +0500

    Delete src directory

--- src/store/auth/types.ts
+++
@@ -1,9 +0,0 @@
-export interface AuthState {
-  isAuthenticated: boolean;
-  user: any;
-  users: any[];
-}
-
-export interface AppState {
-  app: AuthState;
-}
--- src/store/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './auth/authReducer';
-import { AppState } from './auth/types';
-
-const rootReducer = combineReducers<AppState>({
-  app: authReducer,
-});
-
-export default rootReducer;
